487064552643928d94feb8a6672ffab3f46b0cbe
RADISSON lionel <lionel.radisson@gmail.com>
2017-10-06 16:22:32 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const config = require( './config.json' ),
+    express = require( 'express' ),
+    app = express(),
+    http = require( 'http' ).Server( app ),
+    io = require( 'socket.io' )( http );
+
+// Create the HTTP Server
+app.use( express.static( __dirname + '/public' ) );
+app.get( '/', ( req, res ) => {
+    res.sendFile( 'index.html' );
+} );
+http.listen( config.serverPort, () => {
+    console.log( 'listening on', config.serverPort );
+} );
+
+
+
+let sockets = [];
+
+io.on( 'connection' , socket => {
+    console.log( socket.id );
+	sockets.push( socket );
+
+	io.emit( 'connected', { nbUsers: sockets.length } );
+
+	socket.on( 'disconnect', () => {
+		sockets = sockets.filter( s => s.id !== socket.id );
+	} );
+
+    socket.on( 'msg', msg => {
+        // console.log( msg );
+        io.emit( 'newMsg', { id: socket.id, msg } );
+	} );
+} );
---
+++ src/public/main.js
@@ -0,0 +1,24 @@
+window.addEventListener( 'load', e => {
+	document.document.querySelector( '#apprtc' ).style.height = window.innerHeight + 'px';
+	var socket = io();
+
+	socket.on( 'connected', function( data ){
+		console.log( socket.id );
+		console.log( data );
+	} );
+
+    document.body.addEventListener( 'click', function(){
+        var data = 'yo';
+        socket.emit( 'msg', data );
+    }, false );
+
+	socket.on( 'newMsg', function( data ){
+		console.log('test2', data );
+		if( data.id == socket.id ){
+			console.log( 'from me' );
+		}
+		else{
+			console.log( 'from other' );
+		}
+	} );
+} );
